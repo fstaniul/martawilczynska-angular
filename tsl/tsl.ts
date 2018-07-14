6240f46e7d8f2d365a618a1af709049081c9f85a
vip1all <vip.all@hotmail.com>
2018-07-14 15:55:48 +0200

    Made home prettier again

--- tsl/tsl.ts
+++ tsl/tsl.ts
@@ -5,7 +5,6 @@ _('navigation.surgical-procedures');
 _('navigation.about-me');
 _('navigation.contact');
 _('navigation.drive');
-_('navigation.reviews');
 _('navigation.office-and-staff');
 
 _('contact.form.name.error.required');
@@ -22,14 +21,3 @@ _('contact.form.name.placeholder');
 _('contact.form.email.placeholder');
 _('contact.form.phone.placeholder');
 _('contact.form.subject.placeholder');
-
-_('reviews.add.form.name.placeholder');
-_('reviews.add.form.email.placeholder');
-_('reviews.add.form.phone.placeholder');
-
-_('reviews.add.form.name.errors.required');
-
-_('reviews.add.form.email.errors.required');
-_('reviews.add.form.email.errors.email');
-
-_('reviews.add.form.text.errors.required');
